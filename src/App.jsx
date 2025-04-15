66b770d20f005f2070f1760f707a9933b40e54d8
Mariosam2 <marco.mariosaa@gmail.com>
2025-04-16 00:34:46 +0200

    restarting project with new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import Jumbo from "./components/Jumbo";
-import Navbar from "./components/Navbar";
-import ProjectsComponent from "./components/ProjectsComponent";
-
 function App() {
   return (
     <>
-      <Navbar />
       <Jumbo />
-      <ProjectsComponent />
     </>
   );
 }
--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -1,23 +1,7 @@
 import "./Jumbo.css";
-import AvatarPc from "../assets/avatar_pc_nobg.png";
-import { ArrowDownIcon } from "@heroicons/react/24/outline";
+
 const Jumbo = () => {
-  return (
-    <section className="jumbo h-screen">
-      <div className="container mx-auto pt-60">
-        <img className="max-w-20 bg-white rounded-full" src={AvatarPc} alt="" />
-        <h1 className="jumbo-heading text-white">
-          Hi I'm Marco,
-          <br />
-          <span className="text-ms_muted">Junior Web Developer</span>
-        </h1>
-      </div>
-      <div className="scroll flex items-center absolute bottom-0 right-0 m-12 text-ms_muted">
-        <span className="uppercase me-2">Scroll</span>
-        <ArrowDownIcon className="size-10 arrow-down border p-1 rounded-full  stroke-1" />
-      </div>
-    </section>
-  );
+  return <section className="jumbo h-screen"></section>;
 };
 
 export default Jumbo;
