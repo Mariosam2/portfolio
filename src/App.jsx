826886dbe1bd933c0c3ff514bf1f53d3ad68c60c
Mariosam2 <marco.mariosaa@gmail.com>
2025-04-16 15:23:12 +0200

    install react router + starting with new template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Jumbo from "./components/Jumbo";
 function App() {
   return (
     <>
-      <Jumbo />
+      <div className="app h-screen bg-ms_dark z-0 relative overflow-hidden">
+        <div className="light purple z-1 top-1/5 left-1/4"></div>
+        <div className="light dark-purple z-1 top-1/3 left-1/3"></div>
+        <div className="layover glass z-2"></div>
+        <div className="scrolling-text z-3">
+          <div className="RightToLeft">
+            <span>Hi I'm Marco, Junior Web Developer.</span>
+            <span>Hi I'm Marco, Junior Web Developer.</span>
+          </div>
+        </div>
+        <div className="iphone z-4">{/* TODO: render components */}</div>
+      </div>
     </>
   );
 }
--- src/components/Jumbo.jsx
+++
@@ -1,7 +0,0 @@
-import "./Jumbo.css";
-
-const Jumbo = () => {
-  return <section className="jumbo h-screen"></section>;
-};
-
-export default Jumbo;
